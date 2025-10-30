b5dcc4d23e95dfbc1cc4bedd8cb82e5464a71d96
Thomas Heck <t@b128.net>
2025-10-30 18:04:25 +0100

    refactor: simplify movement code

--- src/objects/player.rs
+++ src/objects/player.rs
@@ -6,31 +6,29 @@ pub struct Player;
 
 fn move_player(
     keyboard_input: Res<ButtonInput<KeyCode>>,
-    mut q_player_vel: Query<&mut LinearVelocity, With<Player>>,
+    mut player_vel: Single<&mut LinearVelocity, With<Player>>,
 ) {
     const MOVE_SPEED: f32 = 200.;
 
-    for mut vel in q_player_vel.iter_mut() {
-        let mut direction = Vec2::ZERO;
+    let mut direction = Vec2::ZERO;
 
-        if keyboard_input.pressed(KeyCode::KeyD) {
-            direction += Vec2::new(1.0, 0.0);
-        }
-
-        if keyboard_input.pressed(KeyCode::KeyA) {
-            direction -= Vec2::new(1.0, 0.0);
-        }
-
-        if keyboard_input.pressed(KeyCode::KeyW) {
-            direction += Vec2::new(0.0, 1.0);
-        }
-
-        if keyboard_input.pressed(KeyCode::KeyS) {
-            direction -= Vec2::new(0.0, 1.0);
-        }
-
-        vel.0 = direction.normalize_or_zero() * MOVE_SPEED;
+    if keyboard_input.pressed(KeyCode::KeyD) {
+        direction += Vec2::new(1.0, 0.0);
     }
+
+    if keyboard_input.pressed(KeyCode::KeyA) {
+        direction += Vec2::new(-1.0, 0.0);
+    }
+
+    if keyboard_input.pressed(KeyCode::KeyW) {
+        direction += Vec2::new(0.0, 1.0);
+    }
+
+    if keyboard_input.pressed(KeyCode::KeyS) {
+        direction += Vec2::new(0.0, -1.0);
+    }
+
+    player_vel.0 = direction.normalize_or_zero() * MOVE_SPEED;
 }
 
 pub struct Plugin;
